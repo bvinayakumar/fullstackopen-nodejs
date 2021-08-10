0458ec001c16318a1987d1348635c47c13df6bca
Vinaya Kumar B <vinaya.bendi@man-es.com>
2021-08-10 18:43:01 +0530

    uibuild

--- index.js
+++ index.js
@@ -102,6 +102,8 @@ app.get('/info', (request, response) => {
   )
 })
 
+app.use(express.static('build'))
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
